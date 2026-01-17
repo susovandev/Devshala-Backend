b336e79ecd809f576b46a642235e7c2750100d58
Susovan Dev <susovandas.dev@gmail.com>
2026-01-17 09:43:40 +0530

    feat(app): add body-parser middlewares with request body limit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import type { Application } from 'express';
 import configureRoutes from './routes.js';
+import { REQUEST_BODY_LIMIT } from 'constants/index.js';
 
 export default function initializeApp() {
   const app: Application = express();
+
+  // Body-parser middlewares
+  app.use(express.json({ limit: REQUEST_BODY_LIMIT }));
+  app.use(express.urlencoded({ extended: true, limit: REQUEST_BODY_LIMIT }));
 
   // Routes
   configureRoutes(app);
